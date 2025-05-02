3b0b568ce3293fc41146f2b2c6f73f2d6716e0d4
Ethan <eth4nplays@outlook.my>
2025-05-02 16:55:43 +0800

    TaskClock Online
    Updated to TaskClock v2.1.0-beta

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2728693592"
+    serviceWorkerVersion: "2206054349"
   }
 });
